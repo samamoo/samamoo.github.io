fcbbbeb25b41fde638cc7b9f103cbddcc6c1b37c
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-02-13 19:13:24 -0800

    Created a star background

---
+++ src/Stars.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Stars.css";
+
+export default function Stars () {
+  return (
+    <section className="wrapper">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </section>
+  )
+}
