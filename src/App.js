ad8681583a1be00826d4a13d2e383cd09e756101
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-02-15 02:28:03 -0800

    Added react routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Samantha from "./Samantha";
 import Stars from "./Stars";
 import BackToTop from "./BackToTop";
 import Navbar from "./Navbar";
+import About from "./About";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Samantha/>
-      <Stars/>
-      <BackToTop showBelow={250}/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Stars/>
+        <BackToTop showBelow={250}/>
+
+          <Switch>
+            <Route path="/" exact component={Samantha}/>
+            <Route path="/about" exact component={About}/>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
