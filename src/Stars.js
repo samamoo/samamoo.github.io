b4ad5b6ccfe294e72d0f26fc1d4e8dc756041510
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-02-14 20:59:49 -0800

    Added wrapper to each stars selector to control its rotation speed

--- src/Stars.js
+++ src/Stars.js
@@ -3,10 +3,17 @@ import "./Stars.css";
 
 export default function Stars () {
   return (
-    <section className="wrapper">
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
-    </section>
+    <main>
+      <section className="wrapper">
+        <div id="stars"></div>
+      </section>
+      <section className="wrapper2">
+        <div id="stars2"></div>
+      </section>
+      <section className="wrapper3">
+        <div id="stars3"></div>
+      </section>
+      
+    </main>
   )
 }
