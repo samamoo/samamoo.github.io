516e0ac1cd0ab51924734ef0f7136c9c26862fe6
Samantha Fok <SamanthaFok@Samanthas-MacBook-Pro.local>
2021-02-15 02:23:53 -0800

    Created an About Me page with placeholder text

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./About.css";
+
+export default function About () {
+  return (
+    <main>
+      <h1 className="title" >THIS IS THE ABOUT PAGE</h1>
+    </main>
+  )
+}
